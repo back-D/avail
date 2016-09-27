98c9759500acf34b272dde853c88c4b266317172
Nikita Kuzmin <backd80@gmail.com>
2016-09-27 09:35:39 +0300

    Add backbone.js,start making models and collection

--- js/script.js
+++ js/script.js
@@ -8,18 +8,38 @@ $(document).ready(function () {
         }
     }
 
-    function windowSizeDecrease() {
-        var windowSize = window.innerWidth;
-        if (windowSize < 1200) {
-            $(".sidebar").trigger("sticky_kit:detach");
-        }
-    }
-
     windowSizeIncrease();
-    windowSizeDecrease();
-    $(window).resize(windowSizeIncrease);
+
     $(window).resize(windowSizeDecrease);*/
     $(".sidebar").stick_in_parent();
+
+
+
 });
 
+//----------
+// Models
+//----------
 
+var app = {};
+
+app.Stock = Backbone.Model.extend({
+    defaults: {
+        type: "Акция",
+        title: "",
+        name: "",
+        image: "",
+        description: ""
+    }
+});
+
+//----------
+// Collection
+//----------
+
+app.PostList = Backbone.Collection.extend({
+    model: app.Stock,
+    url: 'https://radiant-taiga-58366.herokuapp.com'
+});
+
+app.postList = new app.PostList();
